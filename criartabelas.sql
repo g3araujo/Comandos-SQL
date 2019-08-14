a7168d9189fe3efe6118727f6a712e06578ee1f7
g3araujo <41293255+g3araujo@users.noreply.github.com>
2019-08-14 14:55:38 -0300

    Create criartabelas.sql

---
+++ criartabelas.sql
@@ -0,0 +1,14 @@
+--
+create table Educa_disciplina (
+descricao varchar (60) not null,
+sigla varchar (4) not null,
+id int not null,
+primary key (id));
+--
+CREATE TABLE Cliente (
+Codigo INT NOT NULL AUTO_INCREMENT,
+Nome VARCHAR (60) NOT NULL,
+Data_Nascimento DATE,
+Telefone CHAR (8),
+PRIMARY KEY (Codigo) );#
+--
